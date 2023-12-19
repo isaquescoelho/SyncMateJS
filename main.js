c917efbb0c05fa9bb4ea4a68c4fc802a75947371
Isaque Coelho <isaquepow12@gmail.com>
2023-12-19 20:23:27 -0300

    feat: Initialize main application window in main.js
    - Defines `createWindow` function to initialize the main window of the Electron app.
    - Sets window dimensions to 800x600 and configures webPreferences with nodeIntegration and contextIsolation settings.
    - On app ready, `createWindow` is invoked to load the main UI from 'src/ui/index.html'.
    - Includes a line to open DevTools for development purposes (to be removed in production).
    - Handles 'window-all-closed' event to quit the application on non-Darwin platforms.
    - Reactivates the main window on 'activate' if no windows are open.

--- main.js
+++ main.js
@@ -9,3 +9,34 @@ const defaultBackupPath = path.join(os.homedir(), 'SyncMateJSBackups');
 if (!fs.existsSync(defaultBackupPath)) {
     fs.mkdirSync(defaultBackupPath, { recursive: true });
 }
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    mainWindow.loadFile(path.join('src', 'ui', 'index.html'));
+    mainWindow.webContents.openDevTools(); // Remova esta linha em produção
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
