7a2d1ded1d6d9c37d3907af7a9d95030adc0d113
Isaque Coelho <isaquepow12@gmail.com>
2023-12-19 20:19:19 -0300

    feat: Enhance file handling with recursive copy function
    - Adds `copyFolderRecursiveSync` function for enhanced file and directory handling.
    - The function efficiently copies individual files when a file path is provided as the source.
    - When a directory path is given, it recursively copies all contents, including all subdirectories and files.
    - Ensures maintenance of the original directory structure in the target location.
    - Significantly improves the application's backup and file management capabilities.

--- src/backend/backup.js
+++ src/backend/backup.js
@@ -5,3 +5,28 @@ function copyFileSync(source, target) {
     let targetFile = path.join(target, path.basename(source));
     fs.writeFileSync(targetFile, fs.readFileSync(source));
 }
+
+function copyFolderRecursiveSync(source, target) {
+    // Se a fonte for um arquivo, apenas copie o arquivo.
+    if (fs.lstatSync(source).isFile()) {
+        copyFileSync(source, target);
+        return;
+    }
+
+    // Se a fonte for um diretório, copie tudo dentro dele.
+    if (!fs.existsSync(target)) {
+        fs.mkdirSync(target, { recursive: true });
+    }
+
+    let files = fs.readdirSync(source);
+    files.forEach(function(file) {
+        let curSource = path.join(source, file);
+
+        if (fs.lstatSync(curSource).isDirectory()) {
+            let curTarget = path.join(target, path.basename(file));
+            copyFolderRecursiveSync(curSource, curTarget);
+        } else {
+            copyFileSync(curSource, target);
+        }
+    });
+}
